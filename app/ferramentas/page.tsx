7198765da6e68c55337cd04a9099ae39bcbd3aa0
macway <pessoalmaruaoliveira@gmail.com>
2025-10-08 16:53:15 -0300

    Update page.tsx

--- app/ferramentas/page.tsx
+++ app/ferramentas/page.tsx
@@ -369,7 +369,7 @@ export default function FerramentasPage() {
 
               <div className="text-center mb-8">
                 <p className="text-red-600 font-bold text-base md:text-lg">
-                  🎁 BÔNUS EXTRA - Suporte pessoal no WHATSAPP para quem adiquirir hoje!
+                EXTRA - Suporte pessoal no WHATSAPP para quem adiquirir hoje!
                 </p>
               </div>
 
